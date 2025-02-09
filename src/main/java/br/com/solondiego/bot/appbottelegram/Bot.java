41d730c8049533d27f5a82714b1497259da57e74
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2025-02-09 09:11:13 -0300

    update

--- src/main/java/br/com/solondiego/bot/appbottelegram/Bot.java
+++ src/main/java/br/com/solondiego/bot/appbottelegram/Bot.java
@@ -1,5 +1,5 @@
 package br.com.solondiego.bot.appbottelegram;
 
 public class Bot {
-    static final String BOT_TOKEN = "5017252412:AAFQydsdG3fdN_S4hZ_dZERBSBAfFuai5XU";
+    static final String BOT_TOKEN = "";
 }
